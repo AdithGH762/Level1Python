ed7510c8cfbd31fde5433363e93743d5b3c8a0b6
AdithGH762 <141307952+AdithGH762@users.noreply.github.com>
2025-01-21 11:52:37 +0530

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,43 @@
+# def main():
+# cs=get_cs()
+# lot=cs_to_lot(cs) # convert connect string to list of tuples
+# print(lot)
+# main()
+# ______________________________________________________________________________________
+# Input:
+# system=s;database=d;username=u;password=p
+
+# Output:
+# [('system','s'),(database','d'),('username','u'),('password','p')]
+
+# ---------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
+
+# def get_cs():
+#     return input("Enter the connecting string : ")
+# def cs_to_lot(cs):
+#     lot=[]
+#     pairs=cs.split(";")
+#     for pair in pairs:
+#         key,value=pair.split("=")
+#         lot.append((key,value))
+#     return lot
+# def main():
+#     cs=get_cs()
+#     result=cs_to_lot(cs)
+#     print(result)
+# print(main())
+
+# -------------------------------------------------------------------------------------
+
+# using list comprehension method
+
+def get_cs():
+    return input("Enter the connecting string : ")
+def cs_to_lot(cs):
+    return [tuple(pair.split("=")) for pair in cs.split(";")]
+def main():
+    cs=get_cs()
+    result=cs_to_lot(cs)
+    return print(result)
+main()
