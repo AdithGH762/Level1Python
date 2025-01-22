0696997784c13e9d4ac16bbf503ad5d52f010b7f
AdithGH762 <141307952+AdithGH762@users.noreply.github.com>
2025-01-22 18:48:48 +0530

    Add files via upload

--- test2.py
+++ test2.py
@@ -8,18 +8,17 @@
 
 # Activity 3
 
-def input_no():
-    a = int(input("Enter the Number: "))
-    b = int(input("Enter the Number: "))
-    return a,b
+# def input_no():
+#     a = int(input("Enter the Number: "))
+#     b = int(input("Enter the Number: "))
+#     return a,b
 
 def add(a,b):
     return a+b
 
-def output(a,b):
-    print(f"The sum is {add(a,b)}")
+def output(a):
+    print(f"The sum is {a}")
 
-def main():
-    a,b=input_no()
-    output(a,b)
+def main():    
+    output(add(a = int(input("Enter the Number: ")),b = int(input("Enter the Number: "))))
 main()
