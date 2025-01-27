925117dc0c0ef5aeec1074d6b805ef6e8e2757ca
AdithGH762 <141307952+AdithGH762@users.noreply.github.com>
2025-01-27 12:22:53 +0530

    Add files via upload

--- test3.py
+++ test3.py
@@ -13,29 +13,29 @@
 # ---------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------
 
-# def get_cs():
-#     return input("Enter the connecting string : ")
-# def cs_to_lot(cs):
-#     lot=[]
-#     pairs=cs.split(";")
-#     for pair in pairs:
-#         key,value=pair.split("=")
-#         lot.append((key,value))
-#     return lot
-# def main():
-#     cs=get_cs()
-#     result=cs_to_lot(cs)
-#     print(result)
-# print(main())
+def get_cs():
+    return input("Enter the connecting string : ")
+def cs_to_lot(cs):
+    lot=[]
+    pairs=cs.split(";")
+    for pair in pairs:
+        key,value=pair.split("=")
+        lot.append((key,value))
+    return lot
+def main():
+    cs=get_cs()
+    result=cs_to_lot(cs)
+    print(result)
+print(main())
 
 # -------------------------------------------------------------------------------------
 
 # using list comprehension method
 
-def get_cs():
-    return input("Enter the connecting string : ")
-def cs_to_lot(cs):
-    return [tuple(pair.split("=")) for pair in cs.split(";")]
-def main():
-    print(cs_to_lot(get_cs()))
-main()
+# def get_cs():
+#     return input("Enter the connecting string : ")
+# def cs_to_lot(cs):
+#     return [tuple(pair.split("=")) for pair in cs.split(";")]
+# def main():
+#     print(cs_to_lot(get_cs()))
+# main()
