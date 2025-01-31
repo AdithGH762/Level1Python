4dc73ceb973e38fe8393adf288802b9182b56752
AdithGH762 <141307952+AdithGH762@users.noreply.github.com>
2025-01-31 09:29:10 +0530

    Add files via upload

--- test5.py
+++ test5.py
@@ -1,7 +1,3 @@
-
-
-# THIS IS USING DICTIONARY COMPREHENSION
-
 def sanitize_input(input_string):
     d={}
     key_value_pairs = input_string.split(";")
@@ -9,10 +5,14 @@ def sanitize_input(input_string):
     for pair in key_value_pairs:
         if '=' in pair:
             key, value = pair.split("=",1)
-            key = ''.join([char for char in key if char.isalnum()])
-            value = ''.join([char for char in value if char.isalnum()])
-            d[f'{key}'] = f'{value}'
+            if not key.isalnum() and not value.isalnum():
+                print(f"Invalid KEY: {key} and VALUE: {value}") 
+            elif not key.isalnum():
+                print(f"Invalid key: {key}")
+            elif not value.isalnum():
+                print(f"Invalid value: {value}")
+            else:
+                d[key] = value
     return d
-user_input = "_syst*em=s;da(ta&base=d;us@er)name=u;pas]sword=p!"
-ot=sanitize_input(user_input)
-print(ot)
+user_input = "system=s;dat#abase=+)d;user=name;password=)p;environment=pro*d"
+print(sanitize_input(user_input))
